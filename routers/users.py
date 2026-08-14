416ec150b8ca367731d736f9ad1fdfefd63a614f
Enfield6 <2026814385@qq.com>
2026-08-14 10:18:35 +0800

    异常处理的补充

--- routers/users.py
+++ routers/users.py
@@ -5,7 +5,8 @@ from starlette import status
 
 from crud import users
 from config.db_conf import get_db
-from schemas.users import UserRequest
+from schemas.users import UserRequest, UserAuthResponse, UserInfoResponse
+from utils.response import success_response
 
 router = APIRouter(prefix="/api/user", tags= ["Users"])
 
@@ -14,20 +15,23 @@ router = APIRouter(prefix="/api/user", tags= ["Users"])
 async def register(user_data: UserRequest, db: AsyncSession = Depends(get_db)):
     existing_user = await users.get_user_by_username(db, user_data.username)
     if existing_user:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Username already exists")
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="用户已存在")
     user = await users.create_user(db, user_data)
     token = await users.create_token(db, user.id)
 
-    return {
-        "code": 200,
-        "message": "register success",
-        "data":{
-            "token": token,
-            "user_info":{
-                "id": user.id,
-                "username": user.username,
-                "bio": user.bio,
-                "avatar": user.avatar,
-            }
-        }
-    }
+    # return {
+    #     "code": 200,
+    #     "message": "register success",
+    #     "data":{
+    #         "token": token,
+    #         "user_info":{
+    #             "id": user.id,
+    #             "username": user.username,
+    #             "bio": user.bio,
+    #             "avatar": user.avatar,
+    #         }
+    #     }
+    # }
+    response_data = UserAuthResponse(token= token, user_info=UserInfoResponse.model_validate(user))
+    return success_response(message="注册成功", data= response_data)
+
--- utils/response.py
+++ utils/response.py
@@ -1,11 +1,16 @@
 from fastapi.responses import JSONResponse
 from fastapi.encoders import jsonable_encoder
-def success_response(message: str = "success", data = None)
+
+
+def success_response(message: str = "success", data = None):
     content = {
         "code": 200,
         "message": message,
         "data": data
     }
-
+    # 目标：把任何的fastapi、Pydantic、ORM对象 都必定按此格式响应
+    # 使用jsonable_encoder可以将任何的fastapi、Pydantic、ORM对象 转换为可json序列化的对象
     return JSONResponse(content=jsonable_encoder(content))
+
+
 
